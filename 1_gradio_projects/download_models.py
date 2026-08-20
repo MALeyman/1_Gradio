15c3507abf6fe3e67556ec4da96244ff566e83d8
MaksimLeyman <lejman.max@yandex.ru>
2026-08-20 15:11:05 +0300

    Update download_models.py

--- 1_gradio_projects/download_models.py
+++ 1_gradio_projects/download_models.py
@@ -1,28 +1,28 @@
 import os
 import zipfile
+import requests
 import gdown
 
 def download_and_extract():
-    url = 'https://drive.google.com/uc?id=13Z5VjNKBO8zGFzC5ML3hMrUW2ynQGhsb'
-    archive_name = 'data.zip'
-    extract_dir = '.'
+    
+    url = 'https://drive.google.com/uc?id=1O_mtPVYWYGrL8Eg14BQ4OIXmUjq6ebqT'
+    gdown.download(url, 'data.zip', quiet=False)
 
-    print("Начинается загрузка тяжелых файлов и моделей из Google Drive...")
-    gdown.download(url, archive_name, quiet=False)
 
-    if not os.path.exists(archive_name):
-        print(" Ошибка: Файл не был скачан.")
-        return
+    # Путь к скачанному архиву
+    zip_path = "data.zip"
+    # Каталог для распаковки (можно указать '.', если нужно в текущую папку)
+    extract_dir = "."
 
-    print("Распаковка архива...")
-    with zipfile.ZipFile(archive_name, 'r') as zip_ref:
-        # Поочередная распаковка файлов для экономии ОЗУ
-        for file in zip_ref.namelist():
-            zip_ref.extract(file, extract_dir)
-            
-    print("Очистка временных файлов...")
-    os.remove(archive_name)
-    print("Все модели успешно загружены и готовы к работе!")
+    # Распаковка архива
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_dir)
+
+    print("Архив распакован")
+
+    # Удаление архива
+    os.remove(zip_path)
+    print("Архив удален")
 
 if __name__ == "__main__":
     download_and_extract()
